2ddf48cc5e1dba513fa7f71c75a942f2dbd8cc10
Roy Ginsel <36151106+RoyGinsel@users.noreply.github.com>
2020-09-27 15:23:34 +0200

    Changes

---
+++ SPA1/SPA1/Client/Preferences.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPA1.Client
+{
+    public class Preferences
+    {
+        public string mainBackgroundColor { get; set; }
+    }
+}
